6188546c8dc1d36f08fdfc2d05cd778a1a267633
captaindancer <799714669@qq.com>
2017-09-17 16:41:03 +0800

    增加程序中自定义sys属性位置和默认值日志位置

--- src/main/java/com/dancer/provalidation/log/LogMain.java
+++ src/main/java/com/dancer/provalidation/log/LogMain.java
@@ -3,14 +3,33 @@ package com.dancer.provalidation.log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 public class LogMain {
 
+	private static long initTime;
+	
+	static{
+		initTime=System.currentTimeMillis();
+		System.out.println(initTime);
+		String path=LogMain.class.getProtectionDomain().getCodeSource().getLocation().getFile();
+		System.out.println(path);
+		//设置系统属性
+//		System.setProperty("datasync.booklist", "/Users/liufeng/logs/datasync/booklist");
+	}
+	
 	private static final Logger LOGGER = LoggerFactory.getLogger(LogMain.class);
 	
+	
 	public static void main(String[] args) {
-		LOGGER.info("info");
+		long startTime=System.currentTimeMillis();
+		System.out.println(startTime);
+		System.out.println(startTime-initTime);
+		LOGGER.info("sys property default value");
 		System.out.println(LOGGER.getName());
+		System.out.println(System.getProperty("user.dir"));
+		System.out.println(System.currentTimeMillis()-startTime);
+		
+		//实验方法是：先在最开始的static块中计算出jar包所在的路径，然后使用System.setProperty设置为系统属性
+		//在log4j2.xml文件中读取系统属性
 	}
 
 }
--- src/main/java/com/dancer/provalidation/number/ModeMain.java
+++ src/main/java/com/dancer/provalidation/number/ModeMain.java
@@ -1,6 +1,11 @@
 package com.dancer.provalidation.number;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ModeMain {
+	
+	private static final Logger LOGGER=LoggerFactory.getLogger(ModeMain.class);
 
 	public static void main(String[] args) {
 		int test=989;
@@ -9,6 +14,7 @@ public class ModeMain {
 		System.out.println(test%1000/100);
 		
 		System.out.println(7/2);
+		LOGGER.warn("lala");
 	}
 
 }
